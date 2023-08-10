dd5cbd91cc0d379eb26daa1c1d1b4a285b84d87b
Mike <522015@mail.ru>
2023-08-10 13:45:42 +0200

    базовая настройка

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "typescript", "lua", "go", "tsx", "javascript" },
+  ensure_installed = { "typescript","python", "lua", "go", "tsx", "javascript" },
 
   sync_install = false,
   auto_install = true,
